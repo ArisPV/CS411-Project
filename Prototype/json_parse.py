0065405c5f656475e0c7e1cb09c06f49275b3712
ArisPV <30635629+ArisPV@users.noreply.github.com>
2018-03-26 20:36:37 -0400

    Add files via upload
    Added the basic JSON parse function. We can adjust it later to gather more information, however for now it returns a list of two variables, the first being the string which the Google API assigned to it, the second being a floating point number of the percent likelihood the image was about that label. You will have to do a function call to the function I made in that file in order to get the list output. We can adjust it easily later for further more complicated use if need be.

---
+++ Prototype/json_parse.py
@@ -0,0 +1,26 @@
+import json
+
+
+# This is just to grab some json txt files for testing purposes. Feel free
+# to comment this out when assembling code.
+json_file = open('jsontest.txt', 'r')
+json_file.close
+
+
+# This function takes the labels from the JSON file and returns the highest
+# result. This can be adjusted later to take in all of the results if need be.
+
+def json_parse_labels(json_file):
+    """ This function returns a string which the Google Image API uses as a
+        label and a float number which is the percent likeliness the API has
+        assigned to the label.
+    """
+    
+    json_data = json.load(json_file)
+    label_annotations = json_data['labelAnnotations']
+    
+    top_label_1_desc = label_annotations[0]['description']
+    top_label_1_score = label_annotations[0]['score']
+
+    return [top_label_1_desc, top_label_1_score]
+
